293279f919162f5a579b9f09812a3c917b347e26
Tunahan Karaman <tunahankaraman@outlook.com.tr>
2022-12-26 18:29:18 +0300

    autofac alt yapısı entegre edilmiştir

---
+++ ECommerceBackend.Business/DependencyResolvers/Autofac/AutofacBusiness.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using ECommerceBackend.Business.Abstract;
+using ECommerceBackend.Business.Concrete;
+using ECommerceBackend.DataAccess.Abstract;
+using ECommerceBackend.DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackend.Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusiness : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<CustomerManager>().As<ICustomerService>().SingleInstance();
+            builder.RegisterType<EfCustomerDal>().As<ICustomerDal>().SingleInstance();
+
+            builder.RegisterType<OrderManager>().As<IOrderService>().SingleInstance();
+            builder.RegisterType<EfOrderDal>().As<IOrderDal>().SingleInstance();
+
+            builder.RegisterType<OrderDetailManager>().As<IOrderDetailService>().SingleInstance();
+            builder.RegisterType<EfOrderDetailDal>().As<IOrderDetailDal>().SingleInstance();
+        }
+    }
+}
