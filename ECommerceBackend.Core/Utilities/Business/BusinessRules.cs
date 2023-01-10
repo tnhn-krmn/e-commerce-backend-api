c06360b575ea9a1f50d10f60a6c80fadde633194
yasinanilboss <yasinabostanci@gmail.com>
2023-01-10 16:32:53 +0300

    aop eklendi

---
+++ ECommerceBackend.Core/Utilities/Business/BusinessRules.cs
@@ -0,0 +1,24 @@
+﻿using ECommerceBackend.Core.Utilities.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackend.Core.Utilities.Business
+{
+    public class BusinessRules
+    {
+        public static IResult Run(params IResult[] logics)
+        {
+            foreach (var logic in logics)
+            {
+                if (!logic.Success)
+                {
+                    return logic;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ ECommerceBackend.Core/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,28 @@
+﻿using Castle.DynamicProxy;
+using ECommerceBackend.Core.Aspects.Autofac.Exception;
+using ECommerceBackend.Core.CrossCuttingConcerns.Logging.Log4Net.Loggers;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceBackend.Core.Utilities.Interceptors
+{
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public Castle.DynamicProxy.IInterceptor[] SelectInterceptors(Type type, MethodInfo method, Castle.DynamicProxy.IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>(true).ToList();
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+
+            classAttributes.AddRange(methodAttributes);
+            classAttributes.Add(new ExceptionLogAspect(typeof(FileLogger)));
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+        }
+    }
+}
